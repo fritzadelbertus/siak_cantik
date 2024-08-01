4526a5358d6c66bee2cfa301604c62df5689f69b
fritzadelbertus <furitsucode@gmail.com>
2024-08-01 14:15:32 +0700

    Logix Fix

--- main/academic/summary/inject.js
+++ main/academic/summary/inject.js
@@ -1,22 +1,26 @@
 
 // Extract Data
 
-var tabel = document.querySelectorAll('.box');
+const tabel = document.querySelectorAll('.box');
 
-var tabel_data = tabel[0]
-var data_mahasiswa_lengkap = tabel_data.querySelectorAll('td')
-var tabel_nilai = tabel[2]
-var keterangan_nilai = tabel_nilai.querySelector('td[colspan="13"]')
+const tabel_data = tabel[0]
+const data_mahasiswa_lengkap = tabel_data.querySelectorAll('td')
+const tabel_nilai = tabel[2].children[0]
 
-var nilai_kumulasi = tabel_nilai.querySelectorAll('td.ce.emph')
+let list_ip = []
 
-var list_ip = []
-for (let i = 0; i < nilai_kumulasi.length/3; i++){
-    list_ip.push(nilai_kumulasi[i*3].innerText)
+for (let i = 2; i < tabel_nilai.children.length-1; i++) {
+    if (tabel_nilai.children[i].children.length <= 3) {
+        list_ip.push('-')
+    } else {
+        list_ip.push(tabel_nilai.children[i].querySelector('td.ce.emph').innerText)
+    }
 }
+
+
 list_ip = list_ip.reverse()
 
-var data_mahasiswa = {
+const data_mahasiswa = {
     npm: data_mahasiswa_lengkap[0].innerText,
     nama: data_mahasiswa_lengkap[1].innerText,
     angkatan: data_mahasiswa_lengkap[2].innerText,
@@ -27,24 +31,31 @@ var data_mahasiswa = {
 }
 
 
-
-
+const generateSMT = (i) => {
+    if (i%3 == 0) {
+        return `SP ${i/3}`
+    } else {
+        return `SMT ${i-(Math.floor(i/3))}`
+    }
+}
 
 // Create Element
 
-var currentSemester = 1
-var handleIP = (p) => {
+const ipCycleCheck = (csmt) => csmt > data_mahasiswa.ip.length ? 1 : csmt == 0 ? data_mahasiswa.ip.length : csmt
+
+let currentSemester = 1
+const handleIP = (p) => {
     currentSemester += p
-    if (currentSemester > data_mahasiswa.ip.length) {
-        currentSemester = 1
-    } else if (currentSemester == 0) {
-        currentSemester = data_mahasiswa.ip.length
+    currentSemester = ipCycleCheck(currentSemester)
+    if (data_mahasiswa.ip[currentSemester-1] == '-') {
+        currentSemester += p
     }
+    currentSemester = ipCycleCheck(currentSemester)
     statusbox.querySelector('.ip .text-ip').innerText = data_mahasiswa.ip[currentSemester-1]
-    statusbox.querySelector('.ip .small').innerText = `SMT ${currentSemester}`
+    statusbox.querySelector('.ip .small').innerText = `${generateSMT(currentSemester)}`
 }
 
-var statusbox = document.createElement('div')
+const statusbox = document.createElement('div')
 statusbox.classList.add('status-box')
 statusbox.innerHTML =`
     <div class="profile_photo">
@@ -75,7 +86,7 @@ statusbox.innerHTML =`
                 <div> 
                     <p>IP</p>
                     <p class='text-ip'>${data_mahasiswa.ip[currentSemester-1]}</p>
-                    <p class='small'>SMT ${currentSemester}</p>
+                    <p class='small'>${generateSMT(currentSemester)}</p>
                 </div>
                 <svg class='next-ip' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                     <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 
